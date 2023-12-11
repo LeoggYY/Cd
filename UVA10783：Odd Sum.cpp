a7e9df87e775867ec475eef2bd97a3c6e745b729
LeoggYY <153478805+LeoggYY@users.noreply.github.com>
2023-12-11 13:26:29 +0800

    Add files via upload

---
+++ UVA10783：Odd Sum.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int  os(int a, int b)
+{
+	int sum = 0;
+	for (int i = a; i <= b; i++)
+	{
+		if (i % 2 == 1)sum = sum + i;
+	}
+	return sum;
+}
+int main()
+
+{
+	//printf("%d",os(1,5));
+	int a, b, c, n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &a);
+		scanf("%d", &b);
+		c = os(a, b);
+		printf("Case %d: %d\n", i, c);
+	}
+
+}
