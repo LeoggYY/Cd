8a57efff4fb2aa5a614c0292c2211b4f828a49ba
LeoggYY <153478805+LeoggYY@users.noreply.github.com>
2023-12-11 13:43:29 +0800

    Add files via upload

---
+++ UVA10170 ：The Hotel with Infinite Rooms.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    // 第一天入住人數,結束日期, 人數計數器
+    int S, D, counter;
+    while (scanf("%d %d", &S, &D) != EOF) {
+        counter = S;
+        while (S < D)
+        {
+            counter ++;
+            S += counter;
+        }
+        printf("%d\n", counter);
+    }
+
+}
